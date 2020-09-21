da0095161fa57d25e34cf35351416c84084af147
Kalyan <kalyan.gottipati@bdiplus.com>
2020-09-21 16:50:11 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ImageViewerComponent} from './image-viewer.component';
+
+export * from './image-viewer.component'
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        ImageViewerComponent
+    ],
+    exports: [
+        ImageViewerComponent,
+    ]
+})
+export class ImageViewerModule {
+    static forRoot(): ModuleWithProviders <ImageViewerModule> {
+        return {
+            ngModule: ImageViewerModule,
+        };
+    }
+}
